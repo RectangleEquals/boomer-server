bb09bbaed525d3afc415abea3f3246ee6a0b29b9
RectangleEquals <RectangleEquals@gmail.com>
2021-07-13 04:00:35 -0700

    testing executables

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const os = require('os');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`<html><body><pre>${os.platform()}</pre></body></html>`);
+app.get('/', (req, res) =>
+{
+  var spawn = require('child_process').spawn,
+  ls = spawn('hello');
+
+  ls.stdout.on('data', function (data) {
+    console.log('stdout: ' + data.toString());
+  });
+  
+  ls.stderr.on('data', function (data) {
+    console.error('stderr: ' + data.toString());
+  });
+  
+  ls.on('exit', function (code) {
+    console.log('child process exited with code ' + code.toString());
+  });
+
+  res.send(`<html><body><pre>check the logs</pre></body></html>`);
 });
 
 app.listen(PORT, () => {
